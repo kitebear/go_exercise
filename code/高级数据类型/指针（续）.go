d4a971e6b08d200c350a13ab2a29d2bfb35c154b
donghaoxie <xie5721072@163.com>
2018-12-28 00:41:07 +0800

    指针 （续）

---
+++ code/高级数据类型/指针（续）.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+type Pet interface {
+	Name() string
+	Age() uint8
+}
+
+type Dog struct {
+	name string
+	age  uint8
+}
+
+func (dog Dog) Name() string {
+	return dog.name
+}
+
+func (dog Dog) Age() uint8 {
+	return dog.age
+}
+
+func main() {
+	myDog := Dog{"Little D", 3}
+	_, ok1 := interface{}(&myDog).(Pet)
+	_, ok2 := interface{}(myDog).(Pet)
+	fmt.Printf("%v, %v\n", ok1, ok2)
+}
