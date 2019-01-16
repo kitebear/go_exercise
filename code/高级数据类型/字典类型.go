ff72cafe7c11432616068dfe8e97f92d54bfba2a
donghaoxie <xie5721072@163.com>
2019-01-16 19:40:21 +0800

    hashmap

--- code/高级数据类型/字典类型.go
+++ code/高级数据类型/字典类型.go
@@ -8,4 +8,17 @@ func main() {
 	mm2["erlang"] = 50
 	mm2["python"] = 0
 	fmt.Printf("%d, %d, %d \n", mm2["scala"], mm2["erlang"], mm2["python"])
+
+	// var cMap map[string]int // 只定义, 此时 cMap 为 nil
+	// fmt.Println(cMap == nil)
+	// cMap["北京"] = 1 // 报错，因为 cMap 为 nil
+
+	// var cMap map[string]int
+
+	cMap := make(map[string]int)
+	cMap["北京"] = 1
+
+	// 指定初始容量
+	cMap = make(map[string]int, 100)
+	cMap["北京"] = 1
 }
