49a028c4de207d604339ec22356f5948f74fe7df
donghaoxie <xie5721072@163.com>
2019-01-18 07:18:30 +0800

    For

--- code/基本流程控制 /for语句.go
+++ code/基本流程控制 /for语句.go
@@ -9,4 +9,31 @@ func main() {
 	for _, v := range []int{1, 2, 3, 4} {
 		fmt.Printf("%d: %s \n", v, map1[v])
 	}
+
+	numbers := []int{1, 2, 3}
+
+	for i, v := range numbers {
+		fmt.Printf("Numbers: i %d, v %d \n", i, v)
+	}
+
+	cityCodes := map[string]int{
+		"北京": 1,
+		"上海": 2,
+	}
+
+	for i, v := range cityCodes {
+		fmt.Printf("%s is %d\n", i, v)
+	}
+
+	for i, v := range numbers {
+		if v == 4 {
+			break
+		}
+
+		if v%2 == 0 {
+			continue
+		}
+
+		fmt.Printf("numbers[%d] is %d\n", i, v)
+	}
 }
