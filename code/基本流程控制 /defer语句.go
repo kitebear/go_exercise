b839a3221795f8d855fa1ab876eba34dd8487c6d
donghaoxie <xie5721072@163.com>
2019-01-18 07:18:25 +0800

    defer

--- code/基本流程控制 /defer语句.go
+++ code/基本流程控制 /defer语句.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"net/http"
+	"os"
 )
 
 func deferIt3() {
@@ -20,7 +23,22 @@ func deferIt2() {
 	}
 }
 
+func panicFunc() {
+	defer func() {
+		if p := recover(); p != nil {
+			fmt.Println("recover panic")
+		}
+	}()
+
+	panic(errors.New("this is test for panic"))
+}
+
 func main() {
+	fmt.Println("before panic")
+
+	panicFunc()
+
+	fmt.Println("after panic")
 	// deferIt2()
 	// deferIt4()
 
@@ -48,4 +66,53 @@ func fibonacci(num int) int {
 		return 1
 	}
 	return fibonacci(num-1) + fibonacci(num-2)
+}
+
+// 使用 defer 关闭 http 请求响应体的 Body
+func closeBody(url string) error {
+	resp, err := http.Get(url)
+	defer resp.Body.Close()
+	// ... do more stuff ...
+
+	return err
+}
+
+// 使用 defer 关闭文件句柄
+func closeFile(filename string) error {
+	f, err := os.Open(filename)
+	defer f.Close()
+	// ... do more stuff ...
+
+	return err
+}
+
+func mutex () float32 {
+	return 1.32
+}
+
+type Customer struct {
+	mutex() float32
+}
+
+// 使用 defer 解锁
+func BillCustomer(c *Customer) {
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
+	// ... do more stuff ...
+
+	return
+}
+
+func panicFunc() {
+	panic(errors.New("this is test for panic"))
+}
+
+// 最终返回的是 4。因为return 2 执行后，变量 i 赋值为 2， 但是随后执行了 defer 函数，i 被赋值为4，所以最终返回结果为4。
+func testDefer() (i int) {
+	defer func() {
+		fmt.Println(i)
+		i = 4
+	}()
+
+	return 2
 }
