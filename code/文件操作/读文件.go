76e49c53bae0c540ab213ed8f9324dcbc5acfb8b
donghaoxie <xie5721072@163.com>
2019-01-19 05:45:57 +0800

    read

--- code/文件操作/读文件.go
+++ code/文件操作/读文件.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"io"
 	"os"
 )
 
@@ -20,9 +22,61 @@ func main() {
 
 	// fmt.Println(string(buf))
 
-	b1 := make([]byte, 2)
+	// Block 3
+	// b1 := make([]byte, 2)
+	// f.Seek(5, 0)
+	// f.Read(b1)
+	// fmt.Println(string(b1))
 
-	f.Seek(5, 0)
-	f.Read(b1)
-	fmt.Println(string(b1))
+	// Block 4
+	// b1 := make([]byte, 2)
+	// f.ReadAt(b1, 5)
+	// fmt.Println(string(b1))
+
+	// Block 5
+	// for i := 0; i < 10; i++ {
+	// 	go func() {
+	// 		buff := make([]byte, 2)
+	// 		// f.Seek(5, 0)
+	// 		// f.Read(buff)
+	// 		f.ReadAt(buff, 5)
+	// 		fmt.Println(string(buff))
+	// 	}()
+	// }
+	// time.Sleep(2 * time.Second)
+
+	// Block 6
+	// buff := make([]byte, 16)
+
+	// for {
+	// 	n, err := f.Read(buff)
+
+	// 	if n == 16 {
+	// 		fmt.Print(string(buff))
+	// 	} else {
+	// 		fmt.Print(string(buff[0:n]))
+	// 	}
+
+	// 	if err == io.EOF {
+	// 		break
+	// 	}
+	// }
+
+	// Block7
+	r := bufio.NewReader(f)
+	totalLine := 0
+
+	for {
+		_, isPrefix, err := r.ReadLine()
+
+		if !isPrefix {
+			totalLine++
+		}
+
+		if err == io.EOF {
+			break
+		}
+	}
+
+	fmt.Println("the total line is:", totalLine)
 }
