f518183491cd08563192ae0de8297a4df0482a25
donghaoxie <xie5721072@163.com>
2019-01-26 19:33:03 +0800

    update selectTest

---
+++ code/基本流程控制 /selectTest.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func fibonacci(c, quit chan int) {
+	x, y := 0, 1
+	for {
+		select {
+		case c <- x:
+			fmt.Println("x, y")
+			x, y = y, x+y
+		case <-quit:
+			fmt.Println("quit")
+			return
+		}
+	}
+}
+
+func main() {
+	c := make(chan int)
+	quit := make(chan int)
+	go func() {
+		fmt.Println("duse")
+		for i := 0; i < 10; i++ {
+			fmt.Println("预读取")
+			fmt.Println(<-c)
+		}
+		quit <- 0
+	}()
+	fmt.Println("fibonacci")
+	fibonacci(c, quit)
+
+	tick := time.Tick(100 * time.Millisecond)
+	boom := time.After(500 * time.Millisecond)
+	for {
+		select {
+		case <-tick:
+			fmt.Println("tick.")
+		case <-boom:
+			fmt.Println("BOOM!")
+			return
+		default:
+			fmt.Println("    .")
+			time.Sleep(50 * time.Millisecond)
+		}
+	}
+}
