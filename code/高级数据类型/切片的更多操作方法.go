2ef9a7628ad46dfe86f6333a3366d7ae3edcbbda
donghaoxie <xie5721072@163.com>
2019-01-27 17:50:26 +0800

    []int

--- code/高级数据类型/切片的更多操作方法.go
+++ code/高级数据类型/切片的更多操作方法.go
@@ -20,4 +20,21 @@ func main() {
 	e4 := (11)
 	fmt.Println(slice5)
 	fmt.Printf("%v, %v, %v\n", e2 == slice5[2], e3 == slice5[3], e4 == slice5[4])
+
+	a := make([]int, 5)
+	printSlice("a", a)
+
+	b := make([]int, 0, 5)
+	printSlice("b", b)
+
+	c := b[:2]
+	printSlice("c", c)
+
+	d := c[2:5]
+	printSlice("d", d)
+}
+
+func printSlice(s string, x []int) {
+	fmt.Printf("%s len=%d cap=%d %v\n",
+		s, len(x), cap(x), x)
 }
